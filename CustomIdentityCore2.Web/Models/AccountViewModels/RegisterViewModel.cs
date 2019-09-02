90f451d52a9fb8951a704877859950f4e8ab3d78
Tony Ochiche <aochiche@msn.com>
2019-09-02 12:56:45 -0500

    Add first, middle and last name field to registerviewmodel class...

--- CustomIdentityCore2.Web/Models/AccountViewModels/RegisterViewModel.cs
+++ CustomIdentityCore2.Web/Models/AccountViewModels/RegisterViewModel.cs
@@ -5,6 +5,21 @@ namespace CustomIdentityCore2.Web.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
