be08de356efd7b0f225e91c41ee9452a1f77acdd
Tony <geekdev01@gmail.com>
2019-09-06 21:26:25 -0500

    Add mvccontrollerinfo class,imvccontrollerdirectory interface...

---
+++ CustomIdentityCore2.Web/Models/MvcControllerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CustomIdentityCore2.Web.Services
+{
+    public class MvcControllerInfo
+    {
+        public string Id => $"{AreaName}:{Name}";
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string AreaName { get; set; }
+        public IEnumerable<MvcControllerInfo> Actions { get; set; }
+
+    }
+}
---
+++ CustomIdentityCore2.Web/Services/IMvcControllerDiscovery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CustomIdentityCore2.Web.Services
+{
+    public interface IMvcControllerDiscovery
+    {
+        IEnumerable<MvcControllerInfo> GetControllers();
+    }
+}
