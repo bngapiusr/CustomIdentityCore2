44a5a2c79334e7c5041f6fafcddfae80b93642be
Tony Ochiche <aochiche@msn.com>
2019-09-10 10:18:22 -0500

    Modified logging to use .NetCore/EF core 2.2 implementation...

--- CustomIdentityCore2.Data/CustomIdentityCoreDbContext.cs
+++ CustomIdentityCore2.Data/CustomIdentityCoreDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using CustomIdentityCore2.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 
@@ -7,19 +8,49 @@ namespace CustomIdentityCore2.Data
 {
     public class CustomIdentityCoreDbContext : DbContext
     {
+        private ILoggerFactory _loggerFactory;
         public CustomIdentityCoreDbContext(DbContextOptions<CustomIdentityCoreDbContext> options): base(options)
         {
         }
 
+        public CustomIdentityCoreDbContext()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder
+                .AddConsole()
+                .AddFilter(DbLoggerCategory.Database.Command.Name,
+                    LogLevel.Information));
+            _loggerFactory = serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
         public DbSet<User> User { get; set; }
         public DbSet<Role> Role { get; set; }
         public DbSet<UserRole> UserRole { get; set; }
 
-        public static readonly LoggerFactory IdentityLoggerFactory
-            = new LoggerFactory(new[] {
-                new ConsoleLoggerProvider( (category, level) 
-                     => category == DbLoggerCategory.Database.Command.Name
-                    && level == LogLevel.Information,true) });
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(_loggerFactory)
+                .EnableSensitiveDataLogging(true);
+        }
+
+        //old way of logging in EFCore 2.0 ...
+        //public static readonly LoggerFactory IdentityLoggerFactory
+        //    = new LoggerFactory(new[] {
+        //        new ConsoleLoggerProvider( (category, level) 
+        //             => category == DbLoggerCategory.Database.Command.Name
+        //            && level == LogLevel.Information,true) });
+
+        //Resource Link... https://msdn.microsoft.com/magazine/mt830355
+
+        //private ILoggerFactory getLoggerFactory()
+        //{
+        //    IServiceCollection serviceCollection = new ServiceCollection();
+        //    serviceCollection.AddLogging(builder => builder
+        //        .AddConsole()
+        //        .AddFilter(DbLoggerCategory.Database.Command.Name,
+        //            LogLevel.Information));
+        //    return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        //}
 
     }
 }
