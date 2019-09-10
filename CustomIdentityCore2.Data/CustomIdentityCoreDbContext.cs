a6c67bd68ab099d032eae0f126bafff55f14ae9e
Tony Ochiche <aochiche@msn.com>
2019-09-10 06:58:56 -0500

    Add Logging to context class...

--- CustomIdentityCore2.Data/CustomIdentityCoreDbContext.cs
+++ CustomIdentityCore2.Data/CustomIdentityCoreDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using CustomIdentityCore2.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace CustomIdentityCore2.Data
 {
@@ -12,5 +14,12 @@ namespace CustomIdentityCore2.Data
         public DbSet<User> User { get; set; }
         public DbSet<Role> Role { get; set; }
         public DbSet<UserRole> UserRole { get; set; }
+
+        public static readonly LoggerFactory IdentityLoggerFactory
+            = new LoggerFactory(new[] {
+                new ConsoleLoggerProvider( (category, level) 
+                     => category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information,true) });
+
     }
 }
